1d71d7c5f74731e3b8c5df6c48b2d1380f334f17
Berhanu <wondomen2@gmail.com>
2023-09-14 17:00:03 +0300

    creat task added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,33 @@
 //import { useState } from 'react'
 import './Styles/App.css'
 import NavBar from'./Components/NavBar.jsx'
+import React, { useState } from 'react';
+import TaskList from './pages/TaskList';
+import TaskForm from './pages/TaskForm';
 
 function App() {
-  
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
+  const handleDeleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
 
   return (
     <>
     
       <NavBar />
+      <div>
+      <h1>Task Manager</h1>
+      <TaskForm onAdd={handleAddTask} />
+      <TaskList tasks={tasks} onDelete={handleDeleteTask} />
+    </div>
 
     
     </>
