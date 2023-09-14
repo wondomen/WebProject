346b46b56e67b1ea86f0a1de66f6dbbaa61a01c5
Amir Ghari <amir.ghari@metropolia.fi>
2023-09-14 18:22:20 +0300

    stash

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,20 @@
 // import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './Styles/App.css'
 import NavBar from'./Components/NavBar.jsx'
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+  // const [tasks, setTasks] = useState([]);
 
-  const handleAddTask = (newTask) => {
-    setTasks([...tasks, newTask]);
-  };
+  // const handleAddTask = (newTask) => {
+  //   setTasks([...tasks, newTask]);
+  // };
 
-  const handleDeleteTask = (index) => {
-    const updatedTasks = [...tasks];
-    updatedTasks.splice(index, 1);
-    setTasks(updatedTasks);
-  };
+  // const handleDeleteTask = (index) => {
+  //   const updatedTasks = [...tasks];
+  //   updatedTasks.splice(index, 1);
+  //   setTasks(updatedTasks);
+  // };
 
 
   return (
