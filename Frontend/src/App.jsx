d26f3130d9d8b95f769a89f805d98809510b4068
Amir Ghari <amir.ghari@metropolia.fi>
2023-09-14 15:16:12 +0300

    Frontend Nav Bar id Done!!

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,21 @@
+//import { useState } from 'react'
+import './Styles/App.css'
+import NavBar from'./Components/NavBar.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+    
+      <NavBar />
+
+    
+    </>
+  )   
+      
+    
+  
+}
+
+export default App
---
+++ Frontend/src/Components/NavBar.jsx
@@ -0,0 +1,30 @@
+// import React from 'react';
+import'../Styles/App.css'
+import {MdOutlineAddTask} from 'react-icons/md'
+
+const Navbar = () => {
+  return (
+    <div className="container">
+      <div className="nav">
+          <div className="nav__logo">
+            <a><MdOutlineAddTask color='#0abfad' size={60}/></a>
+          </div>
+          <div className="nav__menu">
+              <ul className="nav-menu-parent">
+                  <li className="nav-menu-items home"><a className="menu-link" href="#Home">Home</a></li>
+                  <li className="nav-menu-items"><a className="menu-link" href="#About-us">About us</a></li>
+                  <li className="nav-menu-items"><a className="menu-link" href="#contact-us">Contact us</a></li>
+              </ul>
+          </div>
+          <div className="btn-nav">
+              <button className="btn">Sign in</button>
+              <button className="btn ">Register</button>
+          </div>
+      </div>
+    </div>
+
+    
+  );
+}
+
+export default Navbar;
