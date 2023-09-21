c23be126aa19c058592c1a45b96306e5ce21494b
Berhanu <wondomen2@gmail.com>
2023-09-21 18:10:50 +0300

    home page done

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -1,13 +1,25 @@
 // import React from 'react'
 import Navbar from './NavBar'
 import TaskDisplay from './TaskDisplay'
+import '../Styles/App.css'
 
 const Home = () => {
   return (
     <>
       <Navbar />
       {/* <TaskDisplay /> */}
+      <div className="container">
+       <div>
+       <h2>Welcome!</h2>
+          <p>Get things done with Task Manager.</p>
+      </div>
+      <div>
+      <img src="../public/pic1.jpg" alt="Image holder" />
+        
+     </div>
+</div>
     </>
+    
   )
 }
 
