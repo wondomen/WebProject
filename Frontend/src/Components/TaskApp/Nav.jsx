76eb8d4345a36d4353ff0d3985c38fa0294db8ea
MKRadius <caomanhkhai110@gmail.com>
2023-10-01 16:32:10 +0300

    Scrollbar for home page and task display

--- Frontend/src/Components/TaskApp/Nav.jsx
+++ Frontend/src/Components/TaskApp/Nav.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import {MdOutlineAddTask} from 'react-icons/md';
+import { useNavigate } from "react-router-dom";
 
 const TaskNavigation = () => {
+    const navigate = useNavigate();
+
     return (
         <>
-            <div className="logo">
+            <div className="logo" onClick={() => navigate("/")}>
                 <MdOutlineAddTask className="logo-icon" size={60}/>
             </div>
 
