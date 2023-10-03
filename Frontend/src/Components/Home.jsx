dd27c7c16ecec989c9941537490d5527365ae612
Amir Ghari <amir.ghari@metropolia.fi>
2023-10-03 11:24:54 +0300

    front page done except for responsiveness

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -5,37 +5,18 @@ import AboutUs from './AboutUs';
 import ContactUs from './ContactUs';
 import Footer from './Footer';
 // import TaskDisplay from './TaskDisplay'
-import TaskDisplay from './TaskDisplay'
-import '../Styles/App.css'
 
 const Home = () => {
   return (
     <>
       <Navbar />
       {/* <TaskDisplay /> */}
-
       <MainHome />
       <AboutUs />
       <ContactUs />  
       <Footer />
       
-
-      <AboutUs />  
-    
-      <div className="container">
-        <div>
-          <h2>Welcome!</h2>
-          <p>Get things done with Task Manager.</p>
-        </div>
-    
-        <div>
-          <img src="../public/pic1.jpg" alt="Image holder" />
-        </div>
-      </div>
-
-
     </>
-    
   )
 }
 
