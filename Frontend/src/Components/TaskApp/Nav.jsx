1fce7f47af6245c217a79dc648a079ea0a8f3a2a
MKRadius <caomanhkhai110@gmail.com>
2023-10-04 16:48:18 +0300

    Dropdown profile menu

--- Frontend/src/Components/TaskApp/Nav.jsx
+++ Frontend/src/Components/TaskApp/Nav.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import {MdOutlineAddTask} from 'react-icons/md';
 import { useNavigate } from "react-router-dom";
 
-const TaskNavigation = () => {
+const TaskNavigation = ({ callback }) => {
     const navigate = useNavigate();
+    
+    localStorage.setItem("user", "tester");
+
+    const user = localStorage.getItem("user");
 
     return (
         <>
@@ -11,8 +15,8 @@ const TaskNavigation = () => {
                 <MdOutlineAddTask className="logo-icon" size={60}/>
             </div>
 
-            <div className="profile">
-                <p>Profile</p>
+            <div className="profile" onClick={() => callback(true)}>
+                <p>{user}</p>
             </div>
         </>
 
