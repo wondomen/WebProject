0d9519fe1561d605bab51dd8a9b36f271f359f7f
MKRadius <caomanhkhai110@gmail.com>
2023-10-07 18:46:51 +0300

    Added basename

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   
   return (
     <>
-      <BrowserRouter>   
+      <BrowserRouter basename='/'>   
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Login" element={<Login />} />
